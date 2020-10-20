2771eefb3b993359bf6e9dd9c090685479bb3371
Shubham Anand <neverwannafly@Shubhams-MacBook-Pro.local>
2020-10-21 04:36:54 +0530

    fix toast issue

--- app/controllers/profilify_controller.rb
+++ app/controllers/profilify_controller.rb
@@ -1,3 +1,19 @@
 class ProfilifyController < ApplicationController
   before_action :validate_request
+
+  def fetch
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def delete
+
+  end
 end
--- app/javascript/packs/utils/toast.js
+++ app/javascript/packs/utils/toast.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 export const SUCCESS_TOAST = 'SUCCESS_TOAST';
 export const ERROR_TOAST = 'ERROR_TOAST';
